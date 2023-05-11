1044d536bc41b8f887982fdd307db248065813d5
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-11 16:26:51 +0100

    structure of routes done

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,30 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 const usersController = require("../controllers/users");
 
+// POST Register new Users
+router.post("/register", usersController.registerUser);
+
+// POST Login User
+router.post("/login", usersController.loginUser);
+
+// POST Logout User
+router.post("/logout", usersController.logoutUser);
+
+// PUT Refresh auth token
+router.put("/refresh-token", usersController.refreshToken);
+
 // GET ALL Users
+router.get("/users/", usersController.getUsers);
 
 // GET User BY ID
-
-// POST Users
+router.get("/users/:id", usersController.getUserById);
 
 // PUT Users BY ID
+router.put("/users/:id", usersController.updateUserById);
 
 // DELETE Users BY ID
+router.delete("/delete/:id", usersController.deleteUserById);
 
 module.exports = router;
