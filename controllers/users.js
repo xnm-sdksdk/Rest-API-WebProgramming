8d5c914d39f331b5c80d1dadf6de8e38ff152452
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-11 16:27:03 +0100

    Addition of refresh token

--- controllers/users.js
+++ controllers/users.js
@@ -2,12 +2,14 @@ exports.registerUser = (req, res, next) => {};
 
 exports.loginUser = (req, res, next) => {};
 
+exports.logoutUser = (req, res, next) => {};
+
+exports.refreshToken = (req, res, next) => {};
+
 exports.getUsers = (req, res, next) => {};
 
 exports.getUserById = (req, res, next) => {};
 
-exports.updateUser = (req, res, next) => {};
+exports.updateUserById = (req, res, next) => {};
 
 exports.deleteUserById = (req, res, next) => {};
-
-exports.logoutUser = (req, res, next) => {};
