e118e25da9cdc79706965fb7e077d0b9b207090f
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-16 15:58:38 +0100

    Server file

--- server.js
+++ server.js
@@ -1,7 +1,54 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+// Importing Routes
+const userRoutes = require("./routes/users");
+const eventRoutes = require("./routes/events");
+const accommodationRoutes = require("./routes/accommodation");
+const reservationRoutes = require("./routes/reservation");
+const authRoutes = require("./routes/auth");
 
+// Importing Model
+const User = require("./models/users");
+
+// Start the App
 const app = express();
 
+// Setup Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connecting to Mongo
+mongoose.connect("").the((resulter) => {
+  User.findOne().then((user) => {
+    if (!user) {
+      const user = new User({
+        name: "NM",
+        email: "40210260@esmad.ipp.pt",
+        accommodations: {
+          accommodation: [],
+        },
+        events: {
+            event: [],
+        },
+        reservations: {
+            reservation: [],
+        },
+
+      });
+    }
+  });
+});
+
+// Implement the Routes
+app.use("/api/v1/users");
+app.use("/api/v1/events");
+app.use("/api/v1/accommodations");
+app.use("/api/v1/reservations");
+app.use("/api/v1/auth");
+
+// Start the Server Application
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port: ${port}`));
