b0f72974bfa8b7afcfe881973a41aa2501e4bed2
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-16 15:58:31 +0100

    User Model

--- models/users.js
+++ models/users.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true}
-})
+const Schema = mongoose.Schema;
 
-const User = mongoose.model('User', userSchema);
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  password_confirmation: { type: String, required: true },
+  role: { type: String, required: true },
+});
 
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
