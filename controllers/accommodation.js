0885dd347d71d1c5cf81e03d1b7c9a6b4c8b608f
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-17 23:34:16 +0100

    controller accommodation

--- controllers/accommodation.js
+++ controllers/accommodation.js
@@ -1,15 +1,33 @@
+const accommodation = require("../models/accommodation");
 const Accommodation = require("../models/accommodation");
 const User = require("../models/user");
 
 exports.getAccommodations = (req, res, next) => {
-    Accommodation.find().then((accommodation) => {
-        res.status(200).json({ success: true, accommodation})
-    }).catch((err) => {
-        res.status(500).json({ success: false, message: 'Something went wrong. Please try again later.'})
+  Accommodation.find()
+    .then((accommodation) => {
+      res.status(200).json({ success: true, accommodation });
     })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          success: false,
+          message: "Something went wrong. Please try again later.",
+        });
+    });
 };
 
-exports.getAccommodationById = (req, res, next) => {};
+exports.getAccommodationById = (req, res, next) => {
+  const accommodationId = req.params.id;
+  Accommodation.findById(accommodationId).then((accommodation) => {
+    if(!accommodation) {
+        return res.status(404).json({ success: false, message: 'Accommodation not found.'})
+    }
+    res.json(accommodation);
+  }).catch((err) => {
+    res.status(500).json({success: false, message: 'An Internal Error occurred.'})
+  })
+};
 
 exports.createAccommodation = (req, res, next) => {};
 
