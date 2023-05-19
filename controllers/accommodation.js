5f861d4359f17a8f72ca889282d64c9845bfe09a
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-19 13:55:02 +0100

    pagination

--- controllers/accommodation.js
+++ controllers/accommodation.js
@@ -1,11 +1,30 @@
-const accommodation = require("../models/accommodation");
 const Accommodation = require("../models/accommodation");
 const User = require("../models/user");
 
+const ACCOMMODATIONS_PER_PAGE = 6;
+
 exports.getAccommodations = (req, res, next) => {
+  const page = parseInt(req.query.page);
+  const skip = (page - 1) * ACCOMMODATIONS_PER_PAGE;
+  let totalAccommodations;
+
   Accommodation.find()
+    .count()
+    .then((numAccommodations) => {
+      totalAccommodations = numAccommodations;
+      return Accommodation.find().skip(skip).limit(ACCOMMODATIONS_PER_PAGE);
+    })
     .then((accommodation) => {
-      res.status(200).json({ success: true, accommodation });
+      res.status(200).json({
+        success: true,
+        message: accommodation,
+        total: totalAccommodations,
+        hasNextPage: ACCOMMODATIONS_PER_PAGE * page < totalAccommodations,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalAccommodations / ACCOMMODATIONS_PER_PAGE)
+      });
     })
     .catch((err) => {
       res.status(500).json({
@@ -80,21 +99,23 @@ exports.updateAccommodationById = (req, res, next) => {
   const updatedImages = req.body.images;
   const updatedAmenities = req.body.amenities;
 
-  Accommodation.findById(accommodationId).then((accommodation) => {
-    accommodation.title = updatedTitle;
-    accommodation.description = updatedDescription;
-    accommodation.location = updatedLocation;
-    accommodation.price = updatedPrice;
-    accommodation.number_beds = updatedBeds;
-    accommodation.room_type = updatedRoom;
-    accommodation.availability = updatedAvailability;
-    accommodation.images = updatedImages;
-    accommodation.amenities = updatedAmenities;
-    return accommodation.save();
-  }).then((result) => {
-    console.log('Updated Product');
-    res.status(200).json({success:true, message: accommodation})
-  })
+  Accommodation.findById(accommodationId)
+    .then((accommodation) => {
+      accommodation.title = updatedTitle;
+      accommodation.description = updatedDescription;
+      accommodation.location = updatedLocation;
+      accommodation.price = updatedPrice;
+      accommodation.number_beds = updatedBeds;
+      accommodation.room_type = updatedRoom;
+      accommodation.availability = updatedAvailability;
+      accommodation.images = updatedImages;
+      accommodation.amenities = updatedAmenities;
+      return accommodation.save();
+    })
+    .then((result) => {
+      console.log("Updated Product");
+      res.status(200).json({ success: true, message: accommodation });
+    });
 };
 
 exports.deleteAccommodationById = (req, res, next) => {
