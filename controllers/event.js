9f9ac9dbf9ae81c9c79b7f4e2d4c9bd817dd9245
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-19 14:58:33 +0100

    controller

--- controllers/event.js
+++ controllers/event.js
@@ -1,6 +1,36 @@
-const Event = require('../models/event')
+const Event = require("../models/event");
+const User = require("../models/user");
 
-exports.getEvents = (req, res, next) => {};
+const EVENTS_PER_PAGE = 6;
+
+exports.getEvents = (req, res, next) => {
+  const page = parseInt(req.query.page);
+  const skip = (page - 1) * EVENTS_PER_PAGE;
+  let totalEvents;
+
+  Event.find()
+  .count()
+    .then((numEvents) => {
+        totalEvents = numEvents
+        return Event.find().skip(skip).limit(EVENTS_PER_PAGE);
+    })
+      if (numEvents.length === 0) {
+        res
+          .status(404)
+          .json({ success: false, message: "No events were found." });
+      } else {
+        res
+          .status(200)
+          .json({ success: true, page: page, limit: limit, event: event });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Something went wrong. Please try again later.",
+      });
+    });
+};
 
 exports.getEventById = (req, res, next) => {};
 
