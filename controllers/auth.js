37fdfc33fc4f4715549068d699602de629f666a2
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-23 15:01:37 +0100

    hashing password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,35 @@
-exports.register = (req, res, next) => {};
+const bcrypt = require("bcrypt");
+const User = require("../models/users");
 
-exports.login = (req, res, next) => {};
-
-exports.logout = (req, res, next) => {};
-
-exports.refreshToken = (req, res, next) => {};
+exports.postSignUp = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        res.status().json({
+          success: false,
+          message: "Account with the given email already exists.",
+        });
+      }
+      return bcrypt.hash(password, 12).then((hashedPassword) => {
+        const user = new User({
+          email: email,
+          password: hashedPassword,
+          events: { items: [] },
+        });
+        return user.save();
+      });
+    })
+    .then((result) => {
+      res
+        .status(201)
+        .json({ success: true, message: "Account created successfully." });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Something went wrong. Please try again later.",
+      });
+    });
+};
