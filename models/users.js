b8904d34b1afdcd9afa8692767bf60594d020211
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-25 14:27:01 +0100

    user model

--- models/users.js
+++ models/users.js
@@ -3,36 +3,34 @@ const Schema = mongoose.Schema;
 const getDb = require("../utils/database").getDb;
 const Accommodation = require("./accommodation");
 
-module.exports = (mongoose) => {
-  const { Schema } = mongoose;
-
-  const userSchema = new Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    role: { type: String, required: true },
-    password: { type: String, required: true },
-    resetToken: String,
-    resetTokenExpiration: Date,
-    accommodations: [
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  role: { type: String, required: true },
+  password: { type: String, required: true },
+  resetToken: String,
+  resetTokenExpiration: Date,
+  accommodations: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Accommodation",
+    },
+  ],
+  events: {
+    items: [
       {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Accommodation",
+        eventId: {
+          type: Schema.Types.ObjectId,
+          ref: "Event",
+          required: true,
+        },
       },
     ],
-    events: {
-      items: [
-        {
-          eventId: {
-            type: Schema.Types.ObjectId,
-            ref: "Event",
-            required: true,
-          },
-        },
-      ],
-    },
-    date_registered: {
-      type: Date,
-      default: Date.now,
-    },
-  });
-};
+  },
+  date_registered: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
