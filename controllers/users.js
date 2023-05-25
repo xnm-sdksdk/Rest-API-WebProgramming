c17149104ae00a150c9944d331120de4927d8769
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-25 12:57:06 +0100

    Register user

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,8 @@ const Users = require("../models/users");
 const { v4: uuidv4 } = require("uuid");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const config = require("../config/config");
+const User = db.user;
 
 // Create a new user
 exports.registerUser = async (req, res, next) => {
@@ -18,7 +20,14 @@ exports.registerUser = async (req, res, next) => {
         .json({ success: false, message: "Fields are mandatory." });
     }
 
-    await Users.registerUser({})
+    // Needs review
+
+    await Users.registerUser({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 12),
+      role: req.body.role,
+    });
 
     const { name, email, password, password_confirmation, role } = req.body;
     const accommodations = [];
@@ -55,8 +64,22 @@ exports.registerUser = async (req, res, next) => {
 // Login
 exports.loginUser = async (req, res, next) => {
   try {
+    if (!req.body || !req.body.name || !req.body.password) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message: "Name and password must be provided.",
+        });
+    }
+
+    // Verification for the name
+    let user = await Users.findOne({ where: { name: req.body.name } });
+    if (!user) return res.status(404).json({ success: false, message: "User with the given name not found."});
+
+
+    
     const { email, password } = req.body;
-    const user = await Users.findOne({ email });
     if (!user) {
       res.status(404).json({ err: "Account does not exist." });
       return;
