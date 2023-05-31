d5a934dc2d402d631808a9fdf64d80e3acba7a5f
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-31 23:09:01 +0100

    comment location

--- models/location.js
+++ models/location.js
@@ -2,21 +2,21 @@
 // const Schema = mongoose.Schema;
 // const getDb = require("../utils/database").getDb;
 
-module.exports = (mongoose) => {
-  const schema = mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-    },
-    city: {
-      type: String,
-      required: true,
-    },
-    postal_code: {
-      type: Number,
-      required: true,
-    },
-  });
-  const Location = mongoose.model("Location", schema);
-  return Location;
-};
+// module.exports = (mongoose) => {
+//   const schema = mongoose.Schema({
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     city: {
+//       type: String,
+//       required: true,
+//     },
+//     postal_code: {
+//       type: Number,
+//       required: true,
+//     },
+//   });
+//   const Location = mongoose.model("Location", schema);
+//   return Location;
+// };
