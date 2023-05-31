2c200474387483a7bfb6fdf429885962287b72a5
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-31 12:25:46 +0100

    config token

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 const config = {
-    USER: process.env.DB_USER || '40210260',
-    PASSWORD: process.env.DB_PASSWORD || "12345",
-    DB: process.env.DB || "RestApiWP"
+  USER: process.env.DB_USER || "40210260",
+  PASSWORD: process.env.DB_PASSWORD || "12345",
+  DB: process.env.DB || "RestApiWP",
+  SECRET: process.env.SECRET,
 };
 
-config.URL = `mongodb+srv://${config.USER}:${config.PASSWORD}@cluster0.u9z6ifo.mongodb.net/${config.DB}?retryWrites=true&w=majority`
+config.URL = `mongodb+srv://${config.USER}:${config.PASSWORD}@cluster0.u9z6ifo.mongodb.net/${config.DB}?retryWrites=true&w=majority`;
 
 module.exports = config;
-
 
 // mongodb+srv://40210260:<password>@cluster0.u9z6ifo.mongodb.net/?retryWrites=true&w=majority
