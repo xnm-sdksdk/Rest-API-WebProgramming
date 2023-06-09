ea940ea54ced812b7dd4e4a8a1f0cf52e80623b6
xnm-sdksdk <n.mendonca@protonmail.com>
2023-06-09 16:51:32 +0100

    config

--- config/config.js
+++ config/config.js
@@ -8,5 +8,3 @@ const config = {
 config.URL = `mongodb+srv://${config.USER}:${config.PASSWORD}@cluster0.u9z6ifo.mongodb.net/${config.DB}?retryWrites=true&w=majority`;
 
 module.exports = config;
-
-// mongodb+srv://40210260:<password>@cluster0.u9z6ifo.mongodb.net/?retryWrites=true&w=majority
