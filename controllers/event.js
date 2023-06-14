3020a709da0d270d38d769647faa5a492c4a3bd7
xnm-sdksdk <n.mendonca@protonmail.com>
2023-06-14 22:36:16 +0100

    change

--- controllers/event.js
+++ controllers/event.js
@@ -177,13 +177,10 @@ exports.searchEvent = async (req, res, next) => {
 
     const events = await Event.find(query);
 
-    if (!title && events.length === 0) {
-      const error = new Error("No events found.");
-      error.status = 404;
-      throw error;
-      // return res
-      //   .status(404)
-      //   .json({ success: false, message: "No events found." });
+    if (title && events.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "No events found." });
     }
 
     res.status(200).json(events);
