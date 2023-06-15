64c9246256e62a7927d94cdea02eb784820d9de8
xnm-sdksdk <n.mendonca@protonmail.com>
2023-06-15 22:06:55 +0100

    changes

--- controllers/event.js
+++ controllers/event.js
@@ -174,36 +174,31 @@ exports.searchEvent = async (req, res, next) => {
   try {
     const { title, location, date } = req.query;
 
+    console.log("Title:", title);
+    console.log("Location:", location);
+    console.log("Date:", date);
     const query = {};
 
-    if (title) {
-      query.title = { $regex: title, $options: "i" }; // for case insensitive
-    } else {
+    if (!title) {
       return res.status(400).json({
         success: false,
         message: "Event with the given title not found.",
       });
     }
 
+    query.title = { $regex: title, $options: "i" }; // for case insensitive
+
     if (location) {
       query.location = { $regex: location, $options: "i" };
-    } else {
-      return res.status(400).json({
-        success: false,
-        message: "Event with the given location not found.",
-      });
     }
 
     if (date) {
       query.date = { $gte: new Date(date) }; // greater than or equal to
-    } else {
-      return res.status(400).json({
-        success: false,
-        message: "Event with the given date not found.",
-      });
     }
 
     console.log(title);
+
+    console.log("Query:", query);
 
     const events = await Event.find(query);
 
