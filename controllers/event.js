212a077cde4e4fb05eb8ffae6d624f6576bbb4ff
xnm-sdksdk <n.mendonca@protonmail.com>
2023-06-15 15:21:27 +0100

    params

--- controllers/event.js
+++ controllers/event.js
@@ -219,7 +219,7 @@ exports.searchEvent = async (req, res, next) => {
 
 exports.attendEvent = async (req, res, next) => {
   try {
-    const userName = req.body.name;
+    const userName = req.params.name;
     const eventId = req.params.eventId;
     console.log(userName);
     console.log(eventId);
